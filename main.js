aff930362bcb8e289909d02d9f624bca125bd7a5
Taylor Smith <tas0018@auburn.edu>
2021-09-17 13:25:15 -0500

    added mobile menu toggle on click

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 //Sticker Nav bar for desktop
 window.addEventListener("scroll", () => {
-    var desktopNav = document.getElementById("desktop-nav");
-    desktopNav.classList.toggle("sticky", window.scrollY > 0);
-})
+  let desktopNav = document.getElementById("desktop-nav");
+  desktopNav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// On click for mobile nav button
+let mobileMenu = document.getElementById("menu");
+let mobileBars = document.getElementById("mobile-bars");
+let mobileTimes = document.getElementById("mobile-times");
+
+mobileMenu.addEventListener("click", () => {
+  if (mobileBars.style.display === "none") {
+    mobileBars.style.display = "block";
+    mobileTimes.style.display = "none";
+  } else {
+    mobileBars.style.display = "none";
+    mobileTimes.style.display = "block";
+  }
+});
