5dabcf11cca0d9139e786858100fe379dae91dd3
Taylor Smith <tas0018@auburn.edu>
2021-09-18 11:46:34 -0500

    Added animation to the mobile nav on click

--- main.js
+++ main.js
@@ -9,15 +9,20 @@ let mobileMenu = document.getElementById("menu");
 let mobileBars = document.getElementById("mobile-bars");
 let mobileTimes = document.getElementById("mobile-times");
 let content = document.getElementById("content");
+let sideMenu = document.getElementById("side-menu");
 
 mobileMenu.addEventListener("click", () => {
   if (mobileBars.style.display === "none") {
     mobileBars.style.display = "block";
     mobileTimes.style.display = "none";
     content.classList.remove("active");
+
+    sideMenu.classList.remove("slide");
   } else {
     mobileBars.style.display = "none";
     mobileTimes.style.display = "block";
     content.classList.add("active");
+
+    sideMenu.classList.add("slide");
   }
 });
