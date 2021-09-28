c32752d088587744bfac745b704bc664f9807cc4
Taylor Smith <tas0018@auburn.edu>
2021-09-28 15:39:07 -0500

    finished sticky nav

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 //Sticker Nav bar for desktop
-// window.addEventListener("scroll", () => {
-//   let desktopNav = document.getElementById("desktop-nav");
-//   desktopNav.classList.toggle("sticky", window.scrollY > 0);
-// });
-
+if ($(window).width() > 768) {
+  window.addEventListener("scroll", () => {
+    let stickyNav = document.getElementById("sticky-nav");
+    stickyNav.classList.toggle("sticky", window.scrollY > 0);
+  });
+}
 // On click for mobile nav button
 const navSlider = () => {
-  const hamburger = document.querySelector('.hamburger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li')
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-  hamburger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
     //Animate links
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = '';
-      }
-      else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.55}s`
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 5 + 0.55
+        }s`;
       }
     });
     //burger animation
-    hamburger.classList.toggle('toggle');
-
+    hamburger.classList.toggle("toggle");
   });
-
-}
+};
 
 navSlider();
-s
